1e28bbd9cef110e2702c2d0255bf1e3ffe3810ec
631535199 <631535199@qq.com>
2018-04-19 17:16:51 +0800

    2018.4.17

--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,13 @@
 
 let path = require('path');
 let htmlWebpackPlugin = require('html-webpack-plugin');
-let ExtractTextPlugin = require('extract-text-webpack-plugin');
-let extract = new ExtractTextPlugin('build.css');
+
 module.exports={
     entry:{
-        index:'./app/index.js',
-        vendor:['react','react-dom','redux','react-redux','react-router-dom']
+        index:'./app/index.js'
     },
     output:{
-        filename:"[name].js",
+        filename:"bundle.js",
         path:path.resolve('dist')
     },
     module:{
@@ -22,18 +20,10 @@ module.exports={
                 exclude: /node_modules/,
                 loader: 'babel-loader'
             },
-            {test:/\.less$/,use:extract.extract(['css-loader',{
-                loader:'postcss-loader',
-                options:{
-                    plugins:[
-                        require('autoprefixer')//添加前缀
-                    ]
-                }
-            },'less-loader'])}
+            {test:/\.less$/,use:['style-loader','css-loader','less-loader']}
         ]
     },
     plugins:[
-        extract,
         new htmlWebpackPlugin({
             template:'./app/index.html'
         })
